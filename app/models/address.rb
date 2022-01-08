7fbaa5cafc2f2be31818145219f4d7ac38775f17
Sean Doyle <sean.p.doyle24@gmail.com>
2022-01-08 12:58:38 -0500

    Our Starting Point

---
+++ app/models/address.rb
@@ -0,0 +1,26 @@
+class Address < ApplicationRecord
+  with_options presence: true do
+    validates :line_1
+    validates :city
+    validates :postal_code
+  end
+
+  validates :state, inclusion: { in: -> record { record.states.keys }, allow_blank: true },
+                    presence: { if: -> record { record.states.present? } }
+
+  def countries
+    CS.countries.with_indifferent_access
+  end
+
+  def country_name
+    countries[country]
+  end
+
+  def states
+    CS.states(country).with_indifferent_access
+  end
+
+  def state_name
+    states[state]
+  end
+end
